6adeae8a1ebb2e3ef84b0d6b1677c4da1fb29291
HISUIE <32667550+hisuie08@users.noreply.github.com>
2020-10-22 19:31:01 +0900

    embed送信の修正、トークンとチャンネル情報の設定を外部ファイル読み込み

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ from datetime import datetime, timedelta
 import discord
 from discord.ext import commands, tasks
 
+import os
+PATH = os.path.dirname(os.path.abspath(__file__))
 
-ALARM_CHANNEL = "CHANNEL_ID"  # 面倒なのでチャンネルは一元化。大丈夫だ、問題ない。
+TOKEN_PATH = PATH + "/token.txt"
+def token(): return [(f.read(), f.close()) for f in [open(TOKEN_PATH)]][0][0]
+
+
+CH_PATH = PATH + "/channel.txt"
+def alarm_ch(): return [(f.read(), f.close()) for f in [open(CH_PATH)]][0][0]
+
+
+ALARM_CHANNEL = alarm_ch()  # 面倒なのでチャンネルは一元化。大丈夫だ、問題ない。
 
 
 class Alarm_Queue:
@@ -51,7 +61,7 @@ queue = Alarm_Queue()
 bot = commands.Bot("&")
 
 
-@tasks.loop(1)
+@tasks.loop(seconds=1)
 async def check_queue():
     now = int(datetime.timestamp(datetime.now()))
     if queue.queue:
@@ -62,25 +72,36 @@ async def check_queue():
             for target in item["target"]:
                 # Todo for-awaitの検証
                 embed = discord.Embed(title="時間です", color=0x00bfff)
-                embed.add_field("時刻", f"{t_format}")
-                embed.add_field("アラーム名", target[1])
-                await bot.get_channel(ALARM_CHANNEL).send(target[0].mention, embed)
+                embed.add_field(name="時刻", value=f"{t_format}", inline=True)
+                embed.add_field(name="アラーム名", value=target[1], inline=True)
+                send_to = bot.get_channel(int(ALARM_CHANNEL))
+                target_user = bot.get_user(target[0])
+                await send_to.send(target_user.mention, embed=embed)
             del queue.queue[0]
             return
-check_queue.start()
+
+
+@bot.event
+async def on_ready():
+    print(ALARM_CHANNEL)
+    check_queue.start()
+    pass
 
 
 @bot.command()
-async def alarm(ctx, time):
+async def alarm(ctx, time, name="NONE"):
     time_h, time_m = time.split(":")
     if int(time_h) >= 24 or int(time_m) >= 60:
-        await ctx.send(ctx.author.mention + "Time must be lile `12:34`")
+        await ctx.send(ctx.author.mention + f"タイマー設定コマンドの形式は ```&alarm [時(24時間表記):分] [通知するアラーム名(任意)]``` ")
         return
-    target_time = alarm_format(time_h, time_m)
-    target_stamp = int(datetime.timestamp(target_time))
-    queueItem = (target_stamp, ctx.author.id)
-    queue.queue.append(queueItem)
-    queue.queue.sort()
+    target_stamp = int(datetime.timestamp(alarm_format(time_h, time_m)))
+    t_format = f"{time_h}:{time_m}"
+    queueItem = {"time": target_stamp, "target": [(ctx.author.id, name)]}
+    queue.set(queueItem)
+    embed = discord.Embed(title="アラームをセットしました", color=0x00bfff)
+    embed.add_field(name="時刻", value=t_format, inline=True)
+    embed.add_field(name="名前", value=name, inline=True)
+    await ctx.send(embed=embed)
 
 
 @bot.command()
@@ -90,12 +111,15 @@ async def alarms(ctx):
     embed = discord.Embed(
         title=f"{ctx.author.name}#{ctx.author.discriminator} さんのアラーム", color=0x00bfff)
     for item in registered_item:
-        embed.add_field(item[0], item[1])
+        embed.add_field(name=item[0], value=item[1], inline=True)
     # Todo embedのもうちょい
-    await ctx.send(embed)
+    await ctx.send(embed=embed)
 
 
 def alarm_format(time_h, time_m):
+    """
+    時,分を引数にdatetime型を錬成する関数。
+    """
     now = datetime.now()
     if int(time_h) < now.hour or (int(time_h) == now.hour and int(time_m) <= now.minute):
         now += timedelta(days=1)
@@ -104,4 +128,4 @@ def alarm_format(time_h, time_m):
     return result
 
 
-bot.run("とー君")
+bot.run(token(), reconnect=True)
